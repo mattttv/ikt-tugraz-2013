4e1298ef9f0527693866cef540537acbe9194d32
Matthias Vierthaler <matthias.vierthaler@student.tugraz.at>
2013-11-06 21:50:00 +0100

    added template for c-file with include-folder

---
+++ template-c/source/include/util.h
@@ -0,0 +1,7 @@
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#define mmio32(x)   (*(volatile uint32_t *)(x))
+
+#endif
---
+++ template-c/source/main.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include "util.h"
+
+#define GPIO_FSEL_ADDR 0x20200000
+
+#define GPFSEL1_OFFS   4
+#define LED_BIT_SET    18
+
+#define GPSET0_OFFS    28
+#define GPCLR0_OFFS    40
+#define GPIO_PIN_OFFS  16
+
+#define WAIT_DELAY      0x3F0000
+
+//#define mmio32(x) (*(volatile uint32_t*) (x))
+
+void delay_with_loop(uint32_t value);
+
+int main()
+{
+	// set as output
+	mmio32(GPIO_FSEL_ADDR + GPFSEL1_OFFS) |= (1<<LED_BIT_SET);
+	
+	while(1)
+	{
+	
+		// switch on led
+		mmio32(GPIO_FSEL_ADDR + GPCLR0_OFFS) |= (1<<GPIO_PIN_OFFS);
+		
+		// wait 1
+		delay_with_loop(WAIT_DELAY);
+		
+		// switch off led
+		mmio32(GPIO_FSEL_ADDR + GPSET0_OFFS) |= (1<<GPIO_PIN_OFFS);
+		
+		// wait 2
+		delay_with_loop(WAIT_DELAY);
+	
+	}
+}
+
+void delay_with_loop(volatile uint32_t value)
+{
+	for(;  value != 0; value--);
+}
